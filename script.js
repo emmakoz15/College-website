d31bbb52e15ddd70c63b87d37e607f27602e99eb
emmakoz15 <emmakozlovsky@gmail.com>
2024-09-18 14:58:18 -0400

    made main btn function with js

---
+++ script.js
@@ -0,0 +1,17 @@
+//you can make program more efficiant usimg variableas and seeting parameters of program
+
+mbtn.addEventListener("click", () => { 
+    document.getElementById('majors').scrollIntoView();
+});//majors
+
+slbtn.addEventListener("click", () => { 
+    document.getElementById('studentlife').scrollIntoView();
+});//sudent life
+
+cbtn.addEventListener("click", () => {
+    document.getElementById('cost').scrollIntoView();
+});//cost
+
+vbtn.addEventListener("click", () => {
+    document.getElementById('visit').scrollIntoView();
+}); //visit
